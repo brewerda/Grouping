a90491be35e5dc17afd217f4a2c33f9fd76db59b
apcs <apcs@mvhs-u528ap02.dcsdk12.org>
2013-12-05 08:53:49 -0700

    Working Code

---
+++ Grouping.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.io.*;
+
+
+public class Grouping {
+String[] students;
+String answer = "";
+
+
+public static void main(String[] args) throws IOException{
+	File file = new File("class.csv");
+	Scanner scan = new Scanner(file);
+	System.out.println("Enter 1 to choose a random student");
+	System.out.println("Enter 2 to create a random group");
+	Scanner start = new Scanner(System.in);
+	int answer = start.nextInt();
+
+	String[] students = scan.nextLine().split(",");
+	Random r = new Random();
+	shuffle(students, r);
+	
+
+	if(answer == 1) {
+		System.out.println(students[0]);
+	} 
+	if(answer == 2) {
+		System.out.println("How many students would you like per group?");
+		int b = start.nextInt();
+		int name = 0;
+
+		for (int i=0; i< students.length; i++) {
+			
+			for (int x=0; x<b; x++) {
+				System.out.print(students[name] + "  ");
+				name++;
+
+				if(name == students.length) {
+					System.exit(1);
+				}
+			}
+			System.out.println();
+			System.out.println();
+		}
+
+
+	// 	for (int c=0; c<students.length-1; c++) {
+	// 		for (int i=0; i<b; i++) {
+	// 			rand = r.nextInt(students.length);
+	// 			System.out.print(students[rand] + " ");
+	// 			c++;
+	// 		}
+	// 		c--;
+	// 		System.out.println();
+	// 	}
+		
+	// }
+
+}
+}
+
+ public static void shuffle(String[] students, Random r) {
+	Random random = new Random();
+	for (int i = students.length - 1; i> 0 ; i--) {
+	        String a = students[i];
+	        int temp = random.nextInt(i);
+	        students[i] = students[temp];
+	        students[temp] = a;
+	}
+}
+
+}
+
+
+
